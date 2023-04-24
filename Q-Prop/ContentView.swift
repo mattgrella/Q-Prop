f39b62b85e6215b746693e9416e371bac7ad9b22
mattgrella <grellamb@bc.edu>
2023-04-24 15:10:28 -0400

    Initial Commit

---
+++ Q-Prop/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Q-Prop
+//
+//  Created by Matthew Grella on 4/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Q-Prop/Q_PropApp.swift
@@ -0,0 +1,17 @@
+//
+//  Q_PropApp.swift
+//  Q-Prop
+//
+//  Created by Matthew Grella on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Q_PropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
